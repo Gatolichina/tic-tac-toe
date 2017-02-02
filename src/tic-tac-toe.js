d4ab82236e18b5412f06dc055b417cb617fd5c28
Gatolichina <antonchikg@gmail.com>
2017-02-02 16:20:32 +0300

    TicTacToe:add Math.abs

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,14 +1,26 @@
 class TicTacToe {
     constructor() {
-
+    this.matrix=new Array(3);
+    for(var i=0; i<3; i++){
+      var temp=new Array(3);
+      this.matrix[i]=temp;
+      for(var j=0; j<3; j++){
+       this.matrix[i][j] =null;
+      }
+    }
+     this.player=1;
     }
 
     getCurrentPlayerSymbol() {
-
+      if (this.player==1){return 'x';}
+      else if(this.player==0){ return 'o';}
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+        if(this.matrix[rowIndex][columnIndex]==null){
+          this.matrix[rowIndex][columnIndex]=this.player;
+          this.player=Math.abs((this.player-1));
+        }
     }
 
     isFinished() {
@@ -28,6 +40,7 @@ class TicTacToe {
     }
 
     getFieldValue(rowIndex, colIndex) {
+      return this.matrix[rowIndex][colIndex];
 
     }
 }
