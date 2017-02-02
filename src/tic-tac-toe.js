8f7211270d24f985465b6e9aed7fad222990ddf4
Gatolichina <antonchikg@gmail.com>
2017-02-02 23:05:08 +0300

    TicTacToe:fill getWinner method

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -22,7 +22,6 @@ class TicTacToe {
           this.matrix[rowIndex][columnIndex]=this.player;
           this.counter+=1;
           this.player=Math.abs((this.player-1));
-          console.log( 'ku'+this.counter);
         }
     }
 
@@ -31,14 +30,29 @@ class TicTacToe {
     }
 
     getWinner() {
-
+      var line1=this.matrix[0][0]+this.matrix[0][1]+this.matrix [0][2];
+      var line2=this.matrix[1][0]+this.matrix[1][1]+this.matrix [1][2];
+      var line3=this.matrix[2][0]+this.matrix[2][1]+this.matrix [2][2];
+      var col1=this.matrix[0][0]+this.matrix[1][0]+this.matrix [2][0];
+      var col2=this.matrix[0][1]+this.matrix[1][1]+this.matrix [2][1];
+      var col3=this.matrix[0][2]+this.matrix[1][2]+this.matrix [2][2];
+      var cross1=this.matrix[0][0]+this.matrix[1][1]+this.matrix [2][2];
+      var cross2=this.matrix[2][0]+this.matrix[1][1]+this.matrix [0][2];
+      if ((line1==0)||(line2==0)||(line3==0)||(col1==0)||(col2==0)||(col3==0)||(cross1==0)||(cross2==0)){return 0;}
+      else if ((line1==3)||(line2==3)||(line3==3)||(col1==3)||(col2==3)||(col3==3)||(cross1==3)||(cross2==3)){return 1;}
+           else return null;
     }
 
     noMoreTurns() {
-
-    }
+      if(this.counter==9) {
+        return true;
+      }
+      else {
+        return false; }
+  }
 
     isDraw() {
+
 
     }
 
